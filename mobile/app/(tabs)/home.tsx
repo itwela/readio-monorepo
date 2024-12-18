54f2de9616adc89e1e10eac6cf43dd72fd830821
Itwela Ibomu <36617380+itwela@users.noreply.github.com>
2024-12-18 02:08:03 -0500

    still trying to remove sentry

--- mobile/app/(tabs)/home.tsx
+++ mobile/app/(tabs)/home.tsx
@@ -33,7 +33,6 @@ import { filter } from '@/constants/images';
 import { colors } from '@/constants/tokens';
 import circ from "../../assets/images/fadedOrangeCircle.png"
 import { readioRegularFont, readioBoldFont } from '@/constants/tokens';
-import * as Sentry from '@sentry/react-native';
 
 export default function TabOneScreen() {
 
@@ -339,7 +338,6 @@ export default function TabOneScreen() {
                   </View>
 
           <Text onPress={() => navigation.navigate("radioLoading")} style={styles.heading}>Readio</Text>
-          <Text onPress={ () => { Sentry.captureException(new Error('First error')) }}></Text>
           <View style={{position: "relative", width: "100%"}}>
             <FastImage source={circ} style={[{zIndex: -1, width: 250, height: 300, position: "absolute", alignSelf: "center", top: -70, opacity: 0.7}]} resizeMode='contain'/>
           </View>
