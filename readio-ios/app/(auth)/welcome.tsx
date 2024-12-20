f4b4341ff61fede11e5b9c2b3899977dc16af302
Itwela Ibomu <36617380+itwela@users.noreply.github.com>
2024-12-20 12:56:42 -0500

    added quiz

--- readio-ios/app/(auth)/welcome.tsx
+++ readio-ios/app/(auth)/welcome.tsx
@@ -83,19 +83,48 @@ export default function Welcome() {
         <FastImage source={{uri: bookshelfImg}} style={[{zIndex: -2, opacity: 1, position: 'absolute', width: '100%', height: '100%'}]} resizeMode='cover'/>
         <SafeAreaView style={utilStyle.safeAreaContainer}>
             <View style={styles.container}>
-                <Text style={styles.text}>Welcome</Text>
-                <Text onPress={() => router.push('/(auth)/sign-up')} style={styles.subtext}>
-                Sign Up           
-                </Text>
-                <Text onPress={() => router.push('/(auth)/sign-in')} style={styles.subtext}>
-                Sign In           
-                </Text>
-                <Text onPress={() => router.push('/(auth)/quiz')} style={styles.subtext}>
-                Quiz           
-                </Text>
-                <Text onPress={() => router.push('/(tabs)/home')} style={styles.subtext}>
-                Enter App           
-                </Text>
+           
+                <View style={{ width:'100%', height: '6%', display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+                    
+                    <TouchableOpacity activeOpacity={0.99} onPress={() => router.push('/(auth)/welcome')} style={{display: 'flex', flexDirection: 'row'}}>
+                        <Text style={{fontSize: 40, fontWeight: 'bold', color: colors.readioOrange, fontFamily: readioBoldFont}}>L</Text>
+                        <Text style={{fontSize: 40, fontWeight: 'bold', fontFamily: readioBoldFont, color: colors.readioWhite}}>otus</Text>
+                    </TouchableOpacity>
+                </View>
+
+                <View 
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Text style={styles.title}>
+                    Discover a new way to experience articles, tailored just for you.
+                    </Text>
+                    <View style={{width: '70%'}}>
+
+                    <Text style={styles.subtext}>
+                    Get started by telling us a bit about your interests.            
+                    </Text>
+
+                    </View>
+                </View>
+
+                <View style={{paddingVertical: 20, display: 'flex', alignItems: 'center'}}>
+
+                    <TouchableOpacity activeOpacity={0.9} style={buttonStyle.mainButton} onPress={() => router.push('/(auth)/quiz')}>
+                        <Text style={[buttonStyle.mainButtonText, {color: colors.readioWhite}]}>Get Started</Text>
+                    </TouchableOpacity>
+
+                    <Text style={{marginTop: 10, color: colors.readioWhite, width: '100%', display: 'flex', textAlign: 'center', fontFamily: readioRegularFont}}>Already have an account?</Text>
+                
+                    <TouchableOpacity activeOpacity={0.9} onPress={() => router.push('/(tabs)/home')}>
+                        <Text style={[styles.option]}>Enter App</Text>
+                    </TouchableOpacity>
+
+                </View>
+
+
             </View>
         </SafeAreaView>
         </>
@@ -106,7 +135,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: "space-evenly",
     },
     text: {
         fontSize: 60,
