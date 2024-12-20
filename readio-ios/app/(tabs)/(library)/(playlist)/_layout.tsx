9efa7f405cbb1a1e24cc23cec9989835c5705136
Itwela Ibomu <36617380+itwela@users.noreply.github.com>
2024-12-20 08:03:13 -0500

    redoing repo

---
+++ readio-ios/app/(tabs)/(library)/(playlist)/_layout.tsx
@@ -0,0 +1,14 @@
+import { Stack } from 'expo-router';
+
+export default function PlaylistLayout() {
+  
+    return (
+      // <ThemeProvider value={colorScheme === 'light' ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen name=":playlistId" options={{ headerShown: false }} />
+          <Stack.Screen name="favorites" options={{ headerShown: false }} />
+        </Stack>
+      // </ThemeProvider>
+    );
+}
