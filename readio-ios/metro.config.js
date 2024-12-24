ea5c1b336dffd954daa0da8854adb9568e9db814
Itwela Ibomu <36617380+itwela@users.noreply.github.com>
2024-12-24 14:56:42 -0500

    update metro config

--- readio-ios/metro.config.js
+++ readio-ios/metro.config.js
@@ -2,4 +2,12 @@ const { getDefaultConfig } = require("expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
 
+config.transformer = {
+    ...config.transformer,
+    minifierConfig: {
+        keep_classnames: true, // Preserve class names
+        keep_fnames: true,     // Preserve function names
+    },
+};
+
 module.exports = config;
