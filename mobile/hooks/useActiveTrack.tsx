8d7a2fb789c9017036e4bf1d572b5495f48a3599
Itwela Ibomu <36617380+itwela@users.noreply.github.com>
2025-01-14 17:04:16 -0500

    deleted mobile foldfer

--- mobile/hooks/useActiveTrack.tsx
+++
@@ -1,4 +0,0 @@
-import { Readio } from '@/types/type';
-import { Track } from 'react-native-track-player';
-export declare const useActiveTrack: () => Readio | undefined;
-export declare const [activeTrack, setActiveTrack]: [Readio | undefined, (track: Track) => void];
