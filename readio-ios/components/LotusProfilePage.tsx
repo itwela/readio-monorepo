60cf5f51199f625ce7c6ffcdb7b1d619d7ecab43
Itwela Ibomu <36617380+itwela@users.noreply.github.com>
2025-02-28 17:31:38 -0500

    just realize expo has its own image component duh I'm not really sure why I was using fast image in the first place, but I'm getting rid of it as this is literally the only thing stopping me from being able to build this project and check if yes update even works the weird thing is I have a bill that's already fast problems so I'm not really sure what happened, but I'm getting rid of it completely

--- readio-ios/components/LotusProfilePage.tsx
+++ readio-ios/components/LotusProfilePage.tsx
@@ -12,7 +12,7 @@ import { useLotusUser } from "@/helpers/providers/lotusUserContext";
 import { FontAwesome } from "@expo/vector-icons";
 import { router } from 'expo-router';
 import React, { useEffect, useState } from "react";
-import { Dimensions, Image, KeyboardAvoidingView, Modal, Pressable, RefreshControl, ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { ActivityIndicator, Dimensions, Image, KeyboardAvoidingView, Modal, Pressable, RefreshControl, ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 // FIXME This is causing an error in my build ONLY WHEN I RUN EAS BUILD PREVIEW AND ITS CAUSING IT IN THE BUNDLING JAVASCRIPT SPECIFICALLY
 // import FastImage from "react-native-fast-image";
@@ -22,11 +22,14 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function ProfileScreen() {
 
+  const logoImage = Image.resolveAssetSource(require('@/assets/images/cropblacklogo.png'));
+  
   const { user, setUser, checkSignInStatus, refreshUserData, userUpvoteCount, userArticleCount, userStepCount, needsToRefresh, setNeedsToRefresh, setIsSignedIn, setHasAccount } = useLotusUser()
   const { form, setForm, isArticleModalVisible, wantsToMakeAStudyArticle, setWantsToMakeAStudyArticle, setIsArticleGenerating, setIsStudyModalVisible, setIsArticleModalVisible, setArticleGenerationStatus, setWantsToMakeAnArticle, wantsToMakeAnArticle, articleGenerationStatus } = useLotusModal()
   const [modalMessage, setModalMessage] = useState("")
   const [wantsToEditProfile, setWantsToEditProfile] = useState(false)
   const [isEditModalVisible, setIsEditModalVisible] = useState(false)
+  const [isLoading, setIsLoading] = useState(true);
   // const [articleLength, setArticleLength] = useState(0)
   const { ProgressQueue, animatedStyles, setGenerationStarted, setProgressMessage, generationStarted, progressMessage, handleProgressContainerLayout } = useProgressQueue()
   const { settingsOpen } = useLotusSettings()
@@ -144,9 +147,16 @@ export default function ProfileScreen() {
 
           <Text numberOfLines={1} allowFontScaling={false} style={[styles.text, { width: '100%', padding: 20, }]}>{user?.name}</Text>
 
-          <Animated.View entering={FadeInUp.duration(300)} exiting={FadeOutDown.duration(300)} style={{ marginTop: 10, width: 110, justifyContent: 'center', alignSelf: 'center', height: 110, backgroundColor: colors.readioWhite, borderRadius: 500 }}>
+          <Animated.View entering={FadeInUp.duration(300)} exiting={FadeOutDown.duration(300)} style={{ marginTop: 10, width: 110, justifyContent: 'center', alignSelf: 'center', height: 110, display: 'flex', flexDirection: 'column', alignItems: 'center', alignContent: 'center', backgroundColor: colors.readioWhite, borderRadius: 500 }}>
            {/* FIXME MY ATTEMPTED FIX IN MY BUILD. I WAS ABLE TO BUILD JUST FINE BEFORE I MOVED THIS PAGE OUT OF BEING LIKE A TAB AND INSTEAD I IMPORTED THIS INTO THE LAYOUT INSTEAD AND NOW IT'S SAYING I CAN'T USE FAST IMAGE IN THAT CONTACTS WHEN I TRY TO BUILD AGAIN SO I HAVE SWITCHED THIS TO A NORMAL IMAGE AND INSTEAD, AND I'M GONNA SEE IF THIS WORKS my theory is that there's some native thing that I can't directly import a component with certain other components like fast as it seems to be like it'll only let me do that within the scope of the THE ROUTE ITSELF I HAVE NO ISSUE USING FAST IMAGE ONCE I'M IN TAB/WHATEVER I ONLY STARTED THIS ISSUE WHEN I TOOK THIS OUTSIDE OF THAT AND TRIED TO IMPORT IT INTO MY LAYOUT DIRECTLY NOW IF I CAN'T USE ANY IMAGES AT ALL, THIS IS GONNA BE INT as I will have to import this on every page, but we're just gonna start with this first and hopefully this works. */}
-            <Image source={{ uri: getLocalImageUri('blackLogo') }} style={{ width: 70, height: 70, alignSelf: "center", marginTop: 10, backgroundColor: "transparent" }} resizeMode="cover" />
+           <Image 
+              source={logoImage} 
+              style={{ width: 70, height: 70, alignSelf: "center", marginTop: 10, backgroundColor: "transparent" }} 
+              resizeMode="cover"
+              onLoadStart={() => setIsLoading(true)}
+              onLoadEnd={() => setIsLoading(false)}
+            />
+            {!isLoading && <ActivityIndicator style={{position: 'absolute'}} />}
           </Animated.View>
 
         </View>
